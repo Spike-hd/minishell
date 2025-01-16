bb6e75fc3f23fca24a5adc172a8faea992a7176e
Hadrien Duflos <hduflos@c3r2p10.42perpignan.fr>
2025-01-16 15:48:25 +0100

    error quotes

--- init_av.c
+++ init_av.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_av.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: spike <spike@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hduflos <hduflos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:16:43 by hduflos           #+#    #+#             */
-/*   Updated: 2025/01/15 15:39:22 by spike            ###   ########.fr       */
+/*   Updated: 2025/01/16 15:34:52 by hduflos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,57 +55,126 @@ char	*extract_words(char *s, int i, int start)
 
 int	parse_args(char *s, int *i, int *start)
 {
-	char quote;
+	char	quote;
+	int		in_quote;
 
+	in_quote = 0;
+	// Ignorer les espaces
 	while (s[*i] == ' ')
 		(*i)++;
 	if (s[*i] == '\0')
-		return (0); // => plus d'args juste des espaces
+		return (0); // Plus d'arguments
 
-	*start = *i;
-	while (s[*i] != ' ' && s[*i] != '\0')
+	*start = *i; // Marquer le début de l'argument
+	while (s[*i] != '\0')
 	{
-		if (s[*i] == '\'' || s[*i] == '"')
+		// Vérifier les guillemets
+		if ((s[*i] == '\'' || s[*i] == '"') && !in_quote)
 		{
 			quote = s[*i];
+			in_quote = 1;
 			(*i)++;
-			while (s[*i] != quote && s[*i] != '\0')
-				(*i)++;
-			if (s[*i] == quote)
-				(*i)++;
+			continue;
 		}
-		else
+		else if (s[*i] == quote && in_quote)
+		{
+			in_quote = 0;
 			(*i)++;
+			break; // Fin de la séquence entre guillemets
+		}
+		// Fin d'un argument non entre guillemets
+		else if (!in_quote && s[*i] == ' ')
+			break;
+		(*i)++;
 	}
-	return (1); // => argument trouvé
+	return (1); // Argument trouvé
 }
 
-int	count_args(char *s)
+
+// int	count_args(char *s)
+// {
+// 	int		i;
+// 	int		count;
+// 	char	quote;
+// 	int		in_quote;
+
+// 	count = 0;
+// 	i = 0;
+// 	in_quote = 0;
+// 	while (s[i])
+// 	{
+// 		while (s[i] == ' ')
+// 			i++;
+// 		if (s[i] != '\0')
+// 		{
+// 			count++;
+// 			if ((s[i] == '\'' || s[i] == '"') && !in_quote)
+// 			{
+// 				quote = s[i];
+// 				in_quote = 1;
+// 				i++;
+// 				while (s[i] != quote && s[i] != '\0')
+// 					i++;
+// 				if (s[i] == quote)
+// 				{
+// 					printf("u see me? IN QUOTE ? %d\n", in_quote);
+// 					in_quote = 0;
+// 				}
+// 			}
+// 			while (s[i] != ' ' && s[i] != '\0' && (!in_quote || s[i] != quote))
+// 				i++;
+// 		}
+// 	}
+// 	printf("IN QUOTE ? %d // count ================= %d\n", in_quote, count);
+// 	if (in_quote == 1)
+// 		printf("pb args\n");
+// 	return (count);
+// }
+
+
+int	count_args(char *str)
 {
 	int		i;
 	int		count;
 	char	quote;
+	int		in_quote;
 
 	count = 0;
 	i = 0;
-	while (s[i])
+	in_quote = 0;
+	while (str[i])
 	{
-		while (s[i] == ' ')
+		while (str[i] == ' ')
 			i++;
-		if (s[i] != 0)
+		if (str[i] != '\0')
 		{
 			count++;
-			if (s[i] == '\'' || s[i] == '"')
+			if ((str[i] == '\'' || str[i] == '"') && !in_quote)
 			{
-				quote = s[i];
+				quote = str[i];
 				i++;
-				while (s[i] != quote && s[i] != '\0')
+				in_quote = 1;
+				while (str[i] != quote && str[i])
 					i++;
+				if (str[i] == '\0')
+				{
+					printf("end here normally %d // count ================= %d\n", in_quote, count);
+					return (count);
+				}
+				if (str[i] == quote)
+				{
+					printf("u see me? IN QUOTE ? %d\n", in_quote);
+					in_quote = 0;
+				}
 			}
-			if (s[i] != '\0' && s[i] != ' ')
+			while (str[i] != ' ' && str[i] != '\0' && (!in_quote || str[i] != quote))
 				i++;
-		}
+
+			}
 	}
+	printf("IN QUOTE ? %d // count ================= %d\n", in_quote, count);
+	if (in_quote == 1)
+		printf("pb args\n");
 	return (count);
 }
 
